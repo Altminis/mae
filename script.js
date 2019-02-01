051f01dba07ba424d124a4a1ea1c22bef0e6ab3d
Ruffault <arnaud.ruffault@capgemini.com>
2019-02-01 12:24:05 +0100

    ajout d'un carousel sur les temoignages

--- script.js
+++ script.js
@@ -1,4 +1,98 @@
+class Carousel {
+
+    /**
+     * @param {HTMLElement} element 
+     * @param {Object} options 
+     * @param {number} options.slidesToScroll nombres d'éléments à faire défiler
+     * @param {number} options.slidesVisible nombres d'éléments visibles dans un slide
+     * @param {boolean} options.loop affiche ou non les boutons de scroll
+     */
+    constructor(element, options = {}) {
+        this.element = element;
+        this.options = Object.assign({}, {
+            slidesToScroll: 1,
+            slidesVisible: 1,
+            loop: false
+        }, options);
+        let children = [...element.children];
+        this.currentItem = 0;
+        this.root = this.createDivWithClass('carousel');
+        this.container = this.createDivWithClass('carousel__container');
+        
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
+        this.items = children.map(child => {
+            let item = this.createDivWithClass('carousel__item');
+            item.appendChild(child);
+            this.container.appendChild(item);
+            return item;
+        })
+        this.setStyle();
+        this.createNavigation();
+    }
+
+    /**
+     * @param {string} className 
+     * @returns {HTMLElement}
+     */
+    createDivWithClass(className) {
+        let div = document.createElement('div');
+        div.setAttribute('class', className);
+        return div;
+    }
+
+    /**
+     * Applique les bonnes dimension au carousel
+     */
+    setStyle() {
+        let ratio = this.items.length / this.options.slidesVisible;
+        this.container.style.width = `${ratio * 100}%`;
+        this.items.forEach(item =>  item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%");
+    }
+
+    createNavigation() {
+        let nextButton = this.createDivWithClass('carousel__next');
+        let prevButton = this.createDivWithClass('carousel__prev');
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this));
+    }
+
+    next() {
+        this.goToItem(this.currentItem + this.options.slidesToScroll);
+    }
+
+    prev() {
+        this.goToItem(this.currentItem - this.options.slidesToScroll);
+    }
+
+    /**
+     * Scroll sur l' element ciblé
+     * @param {number} index 
+     */
+    goToItem(index) {
+        if(index < 0) {
+            index = this.items.length - this.options.slidesVisible;
+        } else if ( index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined) {
+            index = 0;
+        }
+        let translateX = index * -100 / this.items.length
+        this.container.style.transform = `translate3d(${translateX}%, 0, 0)`;
+        this.currentItem = index;
+    }
+}
+
+
 window.onload = function () {
+
+    //Carousel//
+    const carousel = document.querySelector('#temoignage');
+
+    new Carousel(carousel, {
+        slidesToScroll: 1,
+        slidesVisible: 3
+    })
     // Variables declaration and initialization
 
     //Variables for the navbar
@@ -18,11 +112,10 @@ window.onload = function () {
         name: 'Veuillez saisir votre nom et prénom',
         contact: 'Vous devez saisir un numéro de téléphone ou une adresse éléctronique'
     }
-    
+
     const nonEmptyRegexp = /^[A-Za-z]+$/;
     const telephoneRegexp = /^\+?[0-9 ._-]+$/
     const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 
 
     // Daily Event
@@ -75,14 +168,14 @@ window.onload = function () {
 
         switch (e.target.name) {
             case 'name':
-                if(!nonEmptyRegexp.exec(name.value)){
+                if (!nonEmptyRegexp.exec(name.value)) {
                     e.target.placeholder = messageError.name;
                     e.target.value = "";
                 }
                 break;
             case 'tel':
             case 'email':
-                if(!(telephoneRegexp.exec(tel.value) || emailRegexp.exec(email.value))){
+                if (!(telephoneRegexp.exec(tel.value) || emailRegexp.exec(email.value))) {
                     e.target.placeholder = messageError.contact;
                     e.target.value = "";
                 }
